d5f20e4f542de9cb195a152456908c80213b3933
RoskiDeluge <roberto.delgado@gmail.com>
2020-02-26 18:30:57 -1000

    removed BrowserRouter

--- foodtruck-app/src/App.js
+++ foodtruck-app/src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { Route, Link, Switch } from "react-router-dom";
 import './App.css';
 import Login from './components/Login';
 
 function App() {
   return (
-    <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
       </div>
-    </Router>
   );
 }
 
