a01b4844d9f31ddb353d084b22181d66b8689062
RoskiDeluge <roberto.delgado@gmail.com>
2020-02-26 18:23:39 -1000

    Added <form>, axiosWithAuth to Login

--- foodtruck-app/src/components/Login.js
+++ foodtruck-app/src/components/Login.js
@@ -1,11 +1,69 @@
-import React from 'react';
-
+import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+import Loader from "react-loader-spinner";
 
 const Login = () => {
-    return (
-        <div>I'm a Login form</div>
-    )
-}
 
-export default Login
+  const [ credentials, setCredentials ] = useState({
+    username: "",
+    password: ""
+  });
 
+  const [ isLoading, setIsLoading ] = useState(false);
+
+  const loading = () => {
+    setIsLoading(true);
+  }
+
+  const handleChange = e => {
+    setCredentials({
+        ...credentials,
+        [e.target.name]: e.target.value
+      });
+  };
+
+  const login = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("/login", credentials)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        // props.history.push("/#");
+      })
+      .catch(err => {
+        localStorage.removeItem("token");
+        console.log("invalid login: ", err);
+      });
+  };
+
+  return (
+    <div>
+      {isLoading && (
+          <div>
+            <Loader type="Puff" color="#204963" height={60} width={60} />
+            <p>Logging you in now...</p>
+          </div>
+        )}
+      <h1>Welcome to Food Truck Tracker</h1>  
+      <form onSubmit={login} >
+        <input
+          type="text"
+          name="username"
+          value={credentials.username}
+          placeholder="username"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          value={credentials.password}
+          onChange={handleChange}
+        />
+        <button onClick={loading}>Log in</button>
+      </form>
+    </div>
+  )
+};
+
+export default Login;
