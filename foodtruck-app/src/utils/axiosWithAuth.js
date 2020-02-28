554ad3aed2302202f7050cb8fea533671a818058
RoskiDeluge <roberto.delgado@gmail.com>
2020-02-27 17:41:35 -1000

    Added token name for Login axiosWithAuth

--- foodtruck-app/src/utils/axiosWithAuth.js
+++ foodtruck-app/src/utils/axiosWithAuth.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 export const axiosWithAuth = () => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem("authorization");
 
   return axios.create({
     headers: {
       Authorization: token
     },
-    // baseURL: "http://localhost:5000/api" To be provided by Christorpher, backend team
+    // baseURL: "http://localhost:5000/api" To be provided by Christopher, backend team
   });
 };
