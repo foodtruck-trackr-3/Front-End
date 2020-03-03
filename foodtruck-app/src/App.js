7ecf0e4992be3be392c8740c93d65d8eff4b099e
RoskiDeluge <roberto.delgado@gmail.com>
2020-03-03 10:39:15 -1000

    Set up store and Provider

--- foodtruck-app/src/App.js
+++ foodtruck-app/src/App.js
@@ -2,12 +2,18 @@ import React, { useState } from 'react';
 import { Route, Link, Switch } from "react-router-dom";
 import ProtectedRoute from './components/ProtectedRoute';
 import './App.css';
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { trucksReducer as reducer } from './reducers';
 import SideDrawer from './components/sideDrawer/SideDrawer';
 import Toolbar from './components/Toolbar/Toolbar'
 import Backdrop from './components/Backdrop/Backdrop'
 import Login from './components/Login';
 import Trucklist from './components/Trucklist';
 import Register from './components/Register';
+
+const store = createStore(reducer, applyMiddleware(thunk));
 
 
 function App() {
@@ -24,17 +30,18 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <main style={{ marginTop: "58px" }}>
-      <Route exact path="/" component={Login} />  
-      <ProtectedRoute exact path="/trucks" component={Trucklist} />
-      <Route exact path="/register" component={Register} />
-      <Toolbar drawerClickHandler={drawerTogglerHandler} />
-      <SideDrawer show={sideDrawerOpener} />
-      <Backdrop drawerClose={drawerCloser} />
-      </main>
-      
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <main style={{ marginTop: "58px" }}>
+        <Route exact path="/" component={Login} />  
+        <ProtectedRoute exact path="/trucks" component={Trucklist} />
+        <Route exact path="/register" component={Register} />
+        <Toolbar drawerClickHandler={drawerTogglerHandler} />
+        <SideDrawer show={sideDrawerOpener} />
+        <Backdrop drawerClose={drawerCloser} />
+        </main>
+      </div>
+    </Provider>
   );
 }
 
